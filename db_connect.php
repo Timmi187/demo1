c5a6aa968ecc3052b8866f64401c122d296423ce
Timmi187 <54154839+Timmi187@users.noreply.github.com>
2022-07-02 16:45:59 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','odss_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('sql11.freemysqlhosting.net','sql11503545','UmgpG25LaW','	sql11503545')or die("Could not connect to mysql".mysqli_error($con));
